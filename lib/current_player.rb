7507a81c8bb1ec51c25a8cad3c5f68d1f08f64ea
Lauren Piontek <piontekle@gmail.com>
2018-10-23 15:16:27 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,12 +2,16 @@ def turn_count(board)
   counter = 0
   board.each do |element|
     if element == "X" || element == "O"
-    counter += 1
+      counter += 1
     end
   end
   return counter
 end
 
 def current_player(board)
-  
+  turns = turn_count(board)
+  if turns % 2 == 0
+    return "O"
+  else
+    return "X"
 end
